fe4a2b63b95d6922363cdf5590ba4206e1861c60
Mason <mmazzaro@acre-access.com>
2023-04-19 09:00:29 -0400

    first solo javascript

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-// get the button element, set the varible named button to the first button in the html file
-var button = document.querySelector("button");
+// get the button element, set the variable named button to the first button in the html file
+var button = document.querySelector("#myButton");
 
 // define the handleClick function which is used in the first button that has the button variable defined to it
 function handleClick(event) {
@@ -14,11 +14,44 @@ function handleClick(event) {
   var message = "Job Name: " + jobName + "<br>" +
                 "Job Description: " + jobDescription + "<br>" +
                 "Job Estimate: $" + jobEstimate;
-document.getElementById("message").innerHTML = message;
+  document.getElementById("message").innerHTML = message;
 }
 
-document.getElementById("myButton").addEventListener("click", myFunction);
+// add a click event listener to the button
+button.addEventListener("click", handleClick);
 
+// define the myFunction function which changes the background color of the body
 function myFunction() {
-  document.body.style.backgroundColor = "red";
+  document.body.style.backgroundColor = "black";
+}
+
+// add a click event listener to the button with id "myButton"
+button.addEventListener("click", myFunction);
+
+var myButton = document.getElementById("myButton");
+
+myButton.addEventListener("click", function() {
+  document.body.style.backgroundColor = "black";
+});
+
+
+function mySecondFunction() {
+  var myMessage = document.getElementById("myMessage");
+  myMessage.style.color = "red";
+}
+
+var button = document.getElementById("myButton");
+button.addEventListener("click", mySecondFunction);
+
+var myMessage = document.getElementById("myMessage");
+myMessage.addEventListener("click", function() {
+  myMessage.style.color = "red";
+});
+
+//my first full javascript code
+var hugeH1 = document.getElementById("hugeH1");
+hugeH1.addEventListener("click", textColorBlue);
+
+function textColorBlue() {
+  hugeH1.style.color = "red";
 }
